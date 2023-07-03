f03473490c780bc30fb7be9c4e762d0a3f2960f3
leegeunil <l92836@naver.com>
2023-07-03 17:21:42 +0900

    결재 요청까지 가능

--- src/main/java/kr/co/gf/approval/controller/ApprovalController.java
+++ src/main/java/kr/co/gf/approval/controller/ApprovalController.java
@@ -1,5 +1,6 @@
 package kr.co.gf.approval.controller;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 
@@ -31,36 +32,11 @@ public class ApprovalController {
 	@RequestMapping(value="/approvalVacationRequest.go")
 	public String vacation(HttpSession session, Model model) {
 		
-		// 로그인한 아이디
-		String loginId = (String)session.getAttribute("loginId");
-		logger.info(loginId);
-		String loginIdName = service.loginIdName(loginId);
-		logger.info(loginIdName);
-		model.addAttribute("loginIdName",loginIdName);
-		
-		// 지점장
-		String manager = service.manager();
-		logger.info(manager);
-		model.addAttribute("manager",manager);
-		
-		// 대표
-		String top_Manager = service.top_Manager();
-		logger.info(top_Manager);
-		model.addAttribute("top_Manager",top_Manager);
-		
-		// 직급
-		String position = service.position(loginId);
-		logger.info(position);
-		model.addAttribute("position",position);
-		
-		int year = calendar.get(Calendar.YEAR);
-	    int month = calendar.get(Calendar.MONTH) + 1;
-	    int day = calendar.get(Calendar.DAY_OF_MONTH);
-	    String write_date =  year + "." + month + "." + day;
-	    logger.info(write_date);
-	    model.addAttribute("write_date", write_date);
-		
-		return "approvalVacationRequest";
+		logger.info("loginId: " + session.getAttribute("loginId"));
+		if (session.getAttribute("loginId") != null && !session.getAttribute("loginId").equals("")) {
+			return"approvalVacationRequest" ;
+		}
+		return "redirect:/";
 	}
 	
 	
@@ -70,40 +46,44 @@ public class ApprovalController {
 
 		// 로그인한 아이디
 		String loginId = (String)session.getAttribute("loginId");
-		logger.info(loginId);
-		String loginIdName = service.loginIdName(loginId);
-		logger.info(loginIdName);
-		model.addAttribute("loginIdName",loginIdName);
+		if (loginId != null && !session.getAttribute("loginId").equals("")) {
+			logger.info(loginId);
+			String loginIdName = service.loginIdName(loginId);
+			logger.info(loginIdName);
+			model.addAttribute("loginIdName",loginIdName);
+			
+			// 지점장
+			String manager = service.manager();
+			logger.info(manager);
+			model.addAttribute("manager",manager);
+			
+			// 대표
+			String top_Manager = service.top_Manager();
+			logger.info(top_Manager);
+			model.addAttribute("top_Manager",top_Manager);
+			
+			// 직급
+			String position = service.position(loginId);
+			logger.info(position);
+			model.addAttribute("position",position);
+			
+		    // 현재 날짜 정보를 변수에 할당
+		    int year = calendar.get(Calendar.YEAR);
+		    int month = calendar.get(Calendar.MONTH) + 1;
+		    int day = calendar.get(Calendar.DAY_OF_MONTH);
+		    String write_date =  year + "." + month + "." + day;
+		    logger.info(write_date);
+		    model.addAttribute("write_date", write_date);
+					
+			return "approvalEventRequest";
+		}
 		
-		// 지점장
-		String manager = service.manager();
-		logger.info(manager);
-		model.addAttribute("manager",manager);
-		
-		// 대표
-		String top_Manager = service.top_Manager();
-		logger.info(top_Manager);
-		model.addAttribute("top_Manager",top_Manager);
-		
-		// 직급
-		String position = service.position(loginId);
-		logger.info(position);
-		model.addAttribute("position",position);
-		
-	    // 현재 날짜 정보를 변수에 할당
-	    int year = calendar.get(Calendar.YEAR);
-	    int month = calendar.get(Calendar.MONTH) + 1;
-	    int day = calendar.get(Calendar.DAY_OF_MONTH);
-	    String write_date =  year + "." + month + "." + day;
-	    logger.info(write_date);
-	    model.addAttribute("write_date", write_date);
-				
-		return "approvalEventRequest";
+		return"redirect:/" ;
 	}
 	
 	// 이벤트신청하기
 	@RequestMapping(value="approvalEventRequest.do")
-	public String eventRequest(@RequestParam HashMap<String,String> params, MultipartFile[] files, HttpSession session) {
+	public String eventRequestW(@RequestParam HashMap<String,String> params, MultipartFile[] files, HttpSession session) {
 		
 		logger.info("emp_no 값 : " + params.get("emp_no"));
 		logger.info("approval 값  : " + params.get("approval"));
@@ -126,16 +106,24 @@ public class ApprovalController {
 	@RequestMapping(value="/approvalAllList.do")
 	public ModelAndView approvalAllList(HttpSession session) {
 		String loginId = (String) session.getAttribute("loginId");
-		logger.info("페이지 이동 : "+loginId );
-		return service.approvalAllList(loginId);
+		if (loginId != null && !session.getAttribute("loginId").equals("")) {
+			logger.info("페이지 이동 : "+loginId );
+			return service.approvalAllList(loginId);                                 
+		}
+		
+		return new ModelAndView("redirect:/" );
 	}
 	
 	// 임시저장 문서함
 	@RequestMapping(value="/approvalSaveList.do")
 	public ModelAndView approvalSaveList(HttpSession session) {
 		String loginId = (String) session.getAttribute("loginId");
-		logger.info("임시저장 문서함" + loginId);
-		return service.approvalSaveList(loginId);
+		if (loginId != null && !session.getAttribute("loginId").equals("")) {
+			logger.info("임시저장 문서함" + loginId);
+			return service.approvalSaveList(loginId);
+		}
+		
+		return new ModelAndView("redirect:/" );
 	}
 	
 	// 지출결의서
@@ -144,34 +132,59 @@ public class ApprovalController {
 
 		// 로그인한 아이디
 		String loginId = (String)session.getAttribute("loginId");
-		logger.info(loginId);
-		String loginIdName = service.loginIdName(loginId);
-		logger.info(loginIdName);
-		model.addAttribute("loginIdName",loginIdName);
-		
-		// 지점장
-		String manager = service.manager();
-		logger.info(manager);
-		model.addAttribute("manager",manager);
-		
-		// 대표
-		String top_Manager = service.top_Manager();
-		logger.info(top_Manager);
-		model.addAttribute("top_Manager",top_Manager);
-		
-		// 직급
-		String position = service.position(loginId);
-		logger.info(position);
-		model.addAttribute("position",position);
-		
-		int year = calendar.get(Calendar.YEAR);
-	    int month = calendar.get(Calendar.MONTH) + 1;
-	    int day = calendar.get(Calendar.DAY_OF_MONTH);
-	    String write_date =  year + "." + month + "." + day;
-	    logger.info(write_date);
-	    model.addAttribute("write_date", write_date);
-		
-		return "approvalExpenseReport";
+		if (loginId != null && !session.getAttribute("loginId").equals("")) {
+			logger.info(loginId);
+			String loginIdName = service.loginIdName(loginId);
+			logger.info(loginIdName);
+			model.addAttribute("loginIdName",loginIdName);
+			
+			// 지점장
+			String manager = service.manager();
+			logger.info(manager);
+			model.addAttribute("manager",manager);
+			
+			// 대표
+			String top_Manager = service.top_Manager();
+			logger.info(top_Manager);
+			model.addAttribute("top_Manager",top_Manager);
+			
+			// 직급
+			String position = service.position(loginId);
+			logger.info(position);
+			model.addAttribute("position",position);
+			
+			int year = calendar.get(Calendar.YEAR);
+		    int month = calendar.get(Calendar.MONTH) + 1;
+		    int day = calendar.get(Calendar.DAY_OF_MONTH);
+		    String write_date =  year + "." + month + "." + day;
+		    logger.info(write_date);
+		    model.addAttribute("write_date", write_date);
+			
+			return "approvalExpenseReport";
+		}
+		return"redirect:/" ;
 	}
+	
+	@RequestMapping(value="approvalExpenseReport.do")
+	public String ExpenseReportW(@RequestParam HashMap<String, String> params, MultipartFile[] files, HttpSession session
+			,@RequestParam(value="briefs[]") ArrayList<String> briefs
+			,@RequestParam(value="price[]") ArrayList<String> price
+			,@RequestParam(value="note[]") ArrayList<String> note) {
+		
+		logger.info("emp_no 값 : " + params.get("emp_no"));
+		logger.info("approval 값  : " + params.get("approval"));
+		logger.info("subject 값  : " + params.get("subject"));
+		logger.info("write_date 값 : " + params.get("write_date"));
+		logger.info("state 값  : " + params.get("state"));
+		logger.info("manager 값 : " + params.get("manager"));
+		logger.info("top_manager 값 : " + params.get("top_manager"));
+		logger.info("briefs 값 : "+briefs);
+		logger.info("price 값 : " + price);
+		logger.info("note 값 : " + note);
+		
+		return service.ExpenseReportW(params,files,briefs,price,note);
+	}
+	
+	
 
 }
--- src/main/java/kr/co/gf/approval/service/ApprovalService.java
+++ src/main/java/kr/co/gf/approval/service/ApprovalService.java
@@ -59,7 +59,7 @@ public class ApprovalService {
 	}
 
 	public String eventRequestWrite(HashMap<String, String> params, MultipartFile[] uploadFiles) {
-String page = "redirect:/approvalAllList.do";
+		String page = "redirect:/approvalAllList.do";
 
 		logger.info("params"+params);
 
@@ -68,17 +68,6 @@ String page = "redirect:/approvalAllList.do";
 		dto.setEmp_no(Integer.parseInt(params.get("emp_no")));
 		dto.setApproval(params.get("approval"));
 		dto.setSubject(params.get("subject"));
-
-		String write_dateString = params.get("write_date"); // 문자열로 된 날짜 값 가져오기
-		// 날짜 포맷 지정 (예: "yyyy.MM.dd")
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-		try {
-		    LocalDate localDate = LocalDate.parse(write_dateString, formatter);
-		    java.sql.Date sqlDate = java.sql.Date.valueOf(localDate);
-		    dto.setWrite_date(sqlDate);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
 		dto.setState(params.get("state"));		
 		dto.setManager(params.get("manager"));
 		dto.setTop_manager(params.get("top_manager"));
@@ -86,11 +75,14 @@ String page = "redirect:/approvalAllList.do";
 		int success = dao.eventRequestWrite(dto);
 		
 		if(success == 1) {
-			//dto.setStart_day(params.get("start_day"));
-			//dto.setFinish_day(params.get("finish_day"));
-			//dto.setReason(params.get("reason");
-			//dto.setEtc(params.get("etc");
-			//dao.eventRequestWrite2(dto);
+			dto.setStart_day(params.get("start_day"));
+			dto.setFinish_day(params.get("finish_day"));
+			dto.setReason(params.get("reason"));
+			dto.setEtc(params.get("etc"));
+			int a_idx = dto.getBoard_num();
+			dto.setA_idx(a_idx);
+			logger.info(""+a_idx);
+			dao.eventRequestWriteDown(dto);
 		}
 		
 		for (MultipartFile file : uploadFiles) {
@@ -139,6 +131,60 @@ public void upload(MultipartFile uploadFile,int board_num) {
 		ArrayList<ApprovalDTO> list = dao.approvalSaveList(loginId);
 		mav.addObject("list",list);
 		return mav;
+	}
+
+	public String ExpenseReportW(HashMap<String, String> params, MultipartFile[] uploadFiles, ArrayList<String> briefs1, ArrayList<String> price1, ArrayList<String> note1) {
+		String page = "redirect:/";
+
+		logger.info("params"+params);
+		logger.info("적요 : "+briefs1);
+		logger.info("금액 : "+price1);
+		logger.info("비고 : "+note1);
+
+		ApprovalDTO dto = new ApprovalDTO();
+		
+		dto.setEmp_no(Integer.parseInt(params.get("emp_no")));
+		dto.setApproval(params.get("approval"));
+		dto.setSubject(params.get("subject"));
+		dto.setState(params.get("state"));		
+		dto.setManager(params.get("manager"));
+		dto.setTop_manager(params.get("top_manager"));
+		
+		int success = dao.ExpenseReportW(dto);
+		
+		if(success == 1) {
+			int a_idx = dto.getBoard_num();
+			dto.setA_idx(a_idx);
+			logger.info(""+a_idx);
+			for (int i = 0; i < briefs1.size(); i++) {
+				
+				logger.info(briefs1.get(i));
+				logger.info(price1.get(i));
+				logger.info(note1.get(i));
+				
+				String briefs = briefs1.get(i);
+				String price = price1.get(i);
+				String note = note1.get(i);
+				
+	            dao.expenseReportWDown(a_idx, briefs, price, note);
+	        }
+		}
+		
+		for (MultipartFile file : uploadFiles) {
+			logger.info("photo 있으면 false, 없으면 true :"+file.isEmpty());
+			if(file.isEmpty()==false) {
+				upload(file,dto.getBoard_num());
+				
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		} 
+		page = "redirect:/approvalAllList.do";
+		return page;
 	}
 
 
