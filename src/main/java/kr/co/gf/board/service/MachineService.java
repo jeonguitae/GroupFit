e3c46c8caf63fe2d0f25db5f4f2ef12d6ed053c9
jeonguitae <dmlxo777@naver.com>
2023-07-07 17:17:12 +0900

    기구관리도 조금이랄까

--- src/main/java/kr/co/gf/board/service/MachineService.java
+++ src/main/java/kr/co/gf/board/service/MachineService.java
@@ -1,5 +1,10 @@
 package kr.co.gf.board.service;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Service;
+
+@Service
+@MapperScan("kr.co.gf.board.dao")
 public class MachineService {
 
 }
